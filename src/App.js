8abc8e4844562989f5c6eef7b471441dbbc037cc
unknown <anju7295>
2024-02-28 13:36:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, { id: Date.now(), text: inputValue }]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const handleEditTodo = (id, newText) => {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, text: newText };
+      }
+      return todo;
+    }));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <h1>TODO LIST</h1>
+        <hr></hr>
+        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="add item..." /><br></br>
+        <button type="button" onClick={handleAddTodo}>ADD</button>
+        <table>
+        <tbody>
+          {todos.map(todo => (
+            <tr key={todo.id}>
+              <td>{todo.text}</td>
+              <td>
+                <button onClick={() => handleDeleteTodo(todo.id)} className="delete">Delete</button>
+                <button onClick={() => handleEditTodo(todo.id, prompt('Enter new text:', todo.text))} className="edit">Edit</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+    </>
   );
 }
 
